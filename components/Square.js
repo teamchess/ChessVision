8c1af07ce0cbf13423e0c64d1fd602cd5e497c91
Jack Hedaya <jackehedaya@gmail.com>
2019-01-21 18:02:53 -0400

    remove useless constructor and color dark/light squares

--- components/Square.js
+++ components/Square.js
@@ -4,37 +4,27 @@ import Piece from "./Piece";
 
 /**
  * Will take in coords and piece.
- * 
- * Board:[
- * [1, 2, 3, 4, 5, 3, 2, 1],
- * [6, 6, 6, 6, 6, 6, 6, 6],
- * [0, 0, 0, 0, 0, 0, 0, 0],
- * [0, 0, 0, 0, 0, 0, 0, 0],
- * [16, 16, 16, 16, 16, 16, 16, 16],
- * [11, 12, 13, 14, 15, 13, 12, 11]]
  */
 export default class Square extends React.Component {
-	constructor(props) {
-		super(props);
+  isDarkSquare() {
+    const { x, y } = this.props.coords;
 
-		this.state = {};
-	}
+    return (y % 2 !== 0 && x % 2 !== 0) || (y % 2 === 0 && x % 2 === 0);
+  }
 
-	render() {
-		return (
-			<View style={styles.square}>
-				<Piece number={this.props.piece} />
-				<Text>{this.props.coord}</Text>
-			</View>
-		);
-	}
+  render() {
+    return (
+      <View style={{ ...styles.square, backgroundColor: this.isDarkSquare() ? "#66A5AD" : "#07575B" }}>
+        <Piece number={this.props.piece} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	square: {
-		flex: 1,
-		backgroundColor: "#F8F8F8"
-	},
-	lightSquare: {},
-	darkSquare: {},
+  square: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center"
+  }
 });
