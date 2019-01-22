689ffac15dff56d6cae58e662cc61016f93c0387
Jack Hedaya <jackehedaya@gmail.com>
2019-01-22 02:07:13 -0400

    render board from props instead of state

--- components/Board.js
+++ components/Board.js
@@ -1,31 +1,18 @@
 import React from "react";
 import { View, Dimensions, StyleSheet } from "react-native";
+
 import Square from "./Square";
+import { parseFEN } from "../Utils";
 
 export default class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      startingBoard: [
-        [11, 12, 13, 14, 15, 13, 12, 11],
-        [16, 16, 16, 16, 16, 16, 16, 16],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [6, 6, 6, 6, 6, 6, 6, 6],
-        [1, 2, 3, 4, 5, 3, 2, 1]
-      ]
-    };
-  }
-
   render() {
+    const board = parseFEN(this.props.fen)
     return (
       <View style={styles.board}>
-        {this.state.startingBoard.map((_, yIndex) => {
+        {board.map((_, yIndex) => {
           return (
             <View style={styles.row} key={yIndex}>
-              {this.state.startingBoard[yIndex].map((piece, xIndex) => {
+              {board[yIndex].map((piece, xIndex) => {
                 return <Square piece={piece} coords={{y: yIndex, x: xIndex}} key={`${(yIndex, xIndex)}`} />;
               })}
             </View>
