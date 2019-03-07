6ad2f06330d2620a4996118807fddd74d1f1b4b8
Ron Laniado <rlaniado24@gmail.com>
2019-03-07 10:11:38 -0500

    Fixed button, moved styling to ui.js

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -1,39 +1,15 @@
 import React from "react";
-import { TouchableOpacity, Text, StyleSheet, Image } from "react-native";
+import { TouchableOpacity, Text, Image } from "react-native";
+import styles from "../../styles/ui";
 
-export default props => {
-  return (
-    <TouchableOpacity style={{ ...styles.button, ...props.style }} onPress={props.onPress}>
-      <Image source={props.source} style={styles.icon} />
-      <Text style={styles.buttonText}>{props.children}</Text>
-    </TouchableOpacity>
-  );
+export default (props) => {
+	return (
+		<TouchableOpacity
+			style={{ ...styles.button, ...props.style }}
+			onPress={props.onPress}
+		>
+			<Image source={props.source} style={styles.icon} />
+			<Text style={styles.buttonText}>{props.children}</Text>
+		</TouchableOpacity>
+	);
 };
-
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: "#373737",
-    justifyContent: "center",
-    alignContent: "center",
-    alignItems: "center",
-    borderRadius: 10,
-    flex: 1,
-    shadowColor: "black",
-    shadowOpacity: 0.25,
-    shadowOffset: {
-      width: 1,
-      height: 1.5
-    },
-    shadowRadius: 3
-  },
-  buttonText: {
-    color: "white",
-    textAlign: "center",
-    position: "absolute"
-  },
-  icon: {
-    flex: 1,
-    position: "absolute",
-    resizeMode: "contain"
-  }
-});
