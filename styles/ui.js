0ebf34977e515324741c522fc3cea93fadd70b6f
Ron Laniado <rlaniado24@gmail.com>
2019-03-07 10:11:44 -0500

    Added button styles

--- styles/ui.js
+++ styles/ui.js
@@ -1,20 +1,43 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
+
+const WIDTH = Dimensions.get("window").width,
+	HEIGHT = Dimensions.get("window").height,
+	uiWidth = WIDTH * 0.95;
 
 export default StyleSheet.create({
-  shadow: {
-    shadowColor: "black",
-    shadowOpacity: 0.25,
-    shadowOffset: {
-      width: 1,
-      height: 1.5
-    },
-    shadowRadius: 3
-  },
-  borderRadius: {
-    borderRadius: 8
-  },
-  centerContent: {
-    justifyContent: "center",
-    alignItems: "center"
-  }
+	shadow: {
+		shadowColor: "black",
+		shadowOpacity: 0.25,
+		shadowOffset: {
+			width: 1,
+			height: 1.5,
+		},
+		shadowRadius: 3,
+	},
+	borderRadius: {
+		borderRadius: 8,
+	},
+	button: {
+		backgroundColor: "#373737",
+		justifyContent: "center",
+		alignItems: "center",
+		borderRadius: 10,
+		// "WIDTH / 4 - 50" causes the button's height and width to be proportionate to the device's screen
+		height: WIDTH / 4 - 50,
+		width: WIDTH / 4 - 50,
+	},
+	buttonText: {
+		color: "white",
+		textAlign: "center",
+		position: "absolute",
+	},
+	icon: {
+		height: 40,
+		width: 40,
+		resizeMode: "contain",
+	},
+	centerContent: {
+		justifyContent: "center",
+		alignItems: "center",
+	},
 });
