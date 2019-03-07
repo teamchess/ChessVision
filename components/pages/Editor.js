8b3d378d093289a1b29c82e833aaa79d7c1427da
Ron Laniado <rlaniado24@gmail.com>
2019-03-07 10:11:21 -0500

    Fixed up buttons

--- components/pages/Editor.js
+++ components/pages/Editor.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Clipboard, TouchableOpacity, TouchableWithoutFeedback, View, Text, Image } from "react-native";
+import {
+	Clipboard,
+	TouchableOpacity,
+	TouchableWithoutFeedback,
+	View,
+	Text,
+	Image,
+} from "react-native";
 
 import Button from "../ui/Button";
 import Board from "../board/Board";
@@ -10,89 +17,121 @@ import { PIECES as PIECES_IMAGES } from "../board/Piece";
 const FEN_DEFAULT = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
 
 export default class Editor extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      fen: FEN_DEFAULT,
-      flipBoard: false,
-      piecePickerColor: "white"
-    };
+		this.state = {
+			fen: FEN_DEFAULT,
+			flipBoard: false,
+			piecePickerColor: "white",
+		};
 
-    this.setPickerColor = this.setPickerColor.bind(this);
-    this.flipBoard = this.flipBoard.bind(this);
-  }
+		this.setPickerColor = this.setPickerColor.bind(this);
+		this.flipBoard = this.flipBoard.bind(this);
+	}
 
-  setPickerColor(color) {
-    this.setState({
-      piecePickerColor: color
-    })
-  }
+	setPickerColor(color) {
+		this.setState({
+			piecePickerColor: color,
+		});
+	}
 
-  flipBoard() {
-    this.setState(prevState => ({
-      flipBoard: !prevState.flipBoard
-    }))
-  }
+	flipBoard() {
+		this.setState((prevState) => ({
+			flipBoard: !prevState.flipBoard,
+		}));
+	}
 
-  render() {
-    return (
-      <View styles={styles.container}>
-        <Board style={styles.board} fen={this.state.fen} flip={this.state.flipBoard} />
-        <FenDisplay fen={this.state.fen} />
-        <PieceSelector piecePickerColor={this.state.piecePickerColor} setPickerColor={this.setPickerColor} />
-        <View style={styles.actionButtonContainer}>
-          <ActionButton source={require("../../assets/icons/reverse.png")} onPress={this.flipBoard}/>
-          <ActionButton source={require("../../assets/icons/reset.png")} />
-          <ActionButton source={require("../../assets/icons/save.png")} />
-          <ActionButton source={require("../../assets/icons/upload.png")} />
-        </View>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View styles={styles.container}>
+				<Board
+					style={styles.board}
+					fen={this.state.fen}
+					flip={this.state.flipBoard}
+				/>
+				<FenDisplay fen={this.state.fen} />
+				<PieceSelector
+					piecePickerColor={this.state.piecePickerColor}
+					setPickerColor={this.setPickerColor}
+				/>
+				<View style={styles.actionButtonContainer}>
+					<Button
+						source={require("../../assets/icons/reverse.png")}
+						onPress={this.flipBoard}
+					/>
+					<Button source={require("../../assets/icons/reset.png")} />
+					<Button source={require("../../assets/icons/save.png")} />
+					<Button source={require("../../assets/icons/upload.png")} />
+				</View>
+				
+        
+			</View>
+		);
+	}
 }
 
-const FenDisplay = props => {
-  return (
-    <View style={styles.fenDisplay}>
-      <Text style={styles.fenText} numberOfLines={1} ellipsizeMode="tail">
-        {props.fen}
-      </Text>
-      <View style={styles.iconWrapper}>
-        <TouchableOpacity onPress={() => Clipboard.setString(props.fen)}>
-          <Image style={styles.copyIcon} source={require("../../assets/icons/copy.png")} />
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+const FenDisplay = (props) => {
+	return (
+		<View style={styles.fenDisplay}>
+			<Text style={styles.fenText} numberOfLines={1} ellipsizeMode="tail">
+				{props.fen}
+			</Text>
+			<View style={styles.iconWrapper}>
+				<TouchableOpacity
+					onPress={() => Clipboard.setString(props.fen)}
+				>
+					<Image
+						style={styles.copyIcon}
+						source={require("../../assets/icons/copy.png")}
+					/>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
 };
 
-const PieceSelector = props => {
-  return (
-    <View style={styles.pieceSelector}>
-      <View style={styles.colorSelector}>
-        <View style={styles.colorSelectorGraphic}>
-          <TouchableWithoutFeedback onPress={() => props.setPickerColor("white")}>
-            <View style={styles.colorSelectorWhite} />
-          </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback onPress={() => props.setPickerColor("black")}>
-            <View style={styles.colorSelectorBlack} />
-          </TouchableWithoutFeedback>
-        </View>
-      </View>
-      <View style={styles.piecePicker}>
-        {["pawn", "bishop", "knight", "rook", "queen", "king"].map(p => (
-          <PieceSelectorPiece color={props.piecePickerColor} source={PIECES_IMAGES[p]} key={p} />
-        ))}
-      </View>
-    </View>
-  );
+const PieceSelector = (props) => {
+	return (
+		<View style={styles.pieceSelector}>
+			<View style={styles.colorSelector}>
+				<View style={styles.colorSelectorGraphic}>
+					<TouchableWithoutFeedback
+						onPress={() => props.setPickerColor("white")}
+					>
+						<View style={styles.colorSelectorWhite} />
+					</TouchableWithoutFeedback>
+					<TouchableWithoutFeedback
+						onPress={() => props.setPickerColor("black")}
+					>
+						<View style={styles.colorSelectorBlack} />
+					</TouchableWithoutFeedback>
+				</View>
+			</View>
+			<View style={styles.piecePicker}>
+				{["pawn", "bishop", "knight", "rook", "queen", "king"].map(
+					(p) => (
+						<PieceSelectorPiece
+							color={props.piecePickerColor}
+							source={PIECES_IMAGES[p]}
+							key={p}
+						/>
+					)
+				)}
+			</View>
+		</View>
+	);
 };
 
-const PieceSelectorPiece = props => {
-  return <Image style={{ ...styles.piecePickerPiece, tintColor: props.color }} source={props.source} />;
+const PieceSelectorPiece = (props) => {
+	return (
+		<Image
+			style={{ ...styles.piecePickerPiece, tintColor: props.color }}
+			source={props.source}
+		/>
+	);
 };
 
-const ActionButton = props => {
-  return <Button style={styles.actionButton} {...props} />
-}
+const ActionButton = (props) => {
+	return <Button style={styles.actionButton} {...props} />;
+};
