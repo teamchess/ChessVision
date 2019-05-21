7700df71b310e5d7bdf5ce7a636c292de901aafd
Ron <laniado.ron@gmail.com>
2019-05-20 23:50:02 -0400

    Progressed further with camera, capture btn exists

--- components/pages/Scan.js
+++ components/pages/Scan.js
@@ -4,22 +4,36 @@ import {
   Dimensions,
   View,
   Text,
-  TouchableOpacity
+  TouchableOpacity,
+  TouchableWithoutFeedback
 } from "react-native";
 import { Camera, Permissions } from "expo";
+import { Ionicons } from "@expo/vector-icons";
+
+const { width: winWidth, height: winHeight } = Dimensions.get("window");
 
 export default class Scan extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       hasCameraPermission: null,
+      photo: null,
     };
   }
 
   async componentDidMount() {
-    const {status} = await Permissions.askAsync(Permissions.CAMERA);
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({ hasCameraPermission: status === "granted" }); //If status is true, the camera permissions will be enabled
   }
+  async captureImage() {
+    this.camera.takePictureAsync({onPictureSaved: this.onPictureSaved});
+    console.log("Image captured!");
+
+    
+  }
+ onPictureSaved = async (photo) => {
+  console.log("Picture saved!");
+ }
   render() {
     const { hasCameraPermission } = this.state;
     if (hasCameraPermission === null) {
@@ -28,18 +42,20 @@ export default class Scan extends React.Component {
       return <Text>No access to camera</Text>;
     } else {
       return (
-        <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1, height: 5, width: 500 }} type={Camera.Constants.Type.back} ratio="16:9" useCamera2Api={true}>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: "transparent",
-                flexDirection: "row"
-              }}
-            >
-            <TouchableOpacity style={styles.capture}>
-              <Text>Capture Photo</Text>
-            </TouchableOpacity>
+        <View>
+          <Camera style={styles.preview} ref={camera => (this.camera = camera)}>
+            <View style={styles.topToolbar}>
+              <TouchableOpacity>
+                <Ionicons name="ios-arrow-round-back" size={25} color="white">{"  "}Back</Ionicons>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.cameraToolbar}>
+              <TouchableOpacity
+                style={styles.capture}
+                onPress={this.captureImage}
+              >
+                <Ionicons name="ios-radio-button-off" size={85} color="white" />
+              </TouchableOpacity>
             </View>
           </Camera>
         </View>
@@ -49,9 +65,28 @@ export default class Scan extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  capture: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  preview: {
+    height: winHeight + 34,
+    width: winWidth,
+    position: "absolute",
+    left: 0,
+    top: 0,
+    right: 0,
+    bottom: 0
   },
+  capture: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cameraToolbar: {
+    justifyContent: "center",
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    marginTop: winHeight - 200
+  },
+  topToolbar: {
+    marginTop: 50,
+    marginLeft: 30,
+    borderRadius: 30,
+  }
 });
