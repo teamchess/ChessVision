a136d6d53f31e81f254f17bd1e66335c384ba583
Jack Hedaya <jackehedaya@gmail.com>
2019-05-22 11:47:04 -0400

    onPictureSaved, fetches local server with base64

--- components/pages/Scan.js
+++ components/pages/Scan.js
@@ -37,11 +37,21 @@ export default class Scan extends React.Component {
   };
 
   onPictureSaved = async photo => {
-    await FileSystem.moveAsync({
-      from: photo.uri,
-      to: `${FileSystem.documentDirectory}photos/${Date.now()}.jpg`
-    });
-    console.log(photo);
+    fetch("http://172.20.10.2:3000", {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, cors, *same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+          'Content-Type': 'application/json',
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrer: 'no-referrer', // no-referrer, *client
+      body: JSON.stringify({image: photo.base64}), // body data type must match "Content-Type" heade
+    }).then(x => console.log(x))
+    .catch(x => console.log(x))
+
   };
 
   openGallery = async () => {
